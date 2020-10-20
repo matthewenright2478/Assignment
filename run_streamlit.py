30780154f3d814d5f33b885e0fed79b7a67338e2
matthewenright2478 <matthew.enright@griffithuni.edu.au>
2020-10-21 08:56:49 +1100

    This is a the first file

---
+++ run_streamlit.py
@@ -0,0 +1,11 @@
+import subprocess
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly as p
+import openpyxl
+import sqlite3
+import pydeck as pdk
+import matplotlib.pyplot as plt
+
+subprocess.call('streamlit run streamlit_mockup.py')
